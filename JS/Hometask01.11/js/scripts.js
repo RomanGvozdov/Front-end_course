4ce7e243625d2452870f87f651131d5a27da665a
RomanGvozdov <romangvozdev00@gmail.com>
2017-11-01 13:15:58 +0200

    changes for factorial

--- JS/Hometask01.11/js/scripts.js
+++ JS/Hometask01.11/js/scripts.js
@@ -1,18 +1,27 @@
 //Task 1
+
 var result = 0;
 for (var i = 1; i <= 100; i++)
 	result+=i;
 console.log(result);
+
 //Task 2
+
 function factorial(n) {
 	if ( n > 1 ){
 		return(n * factorial(n - 1));
 	} else 
 		return(1);
 }
+
 var n = prompt("Введите число");
-console.log(factorial(n));
+var result = 1;
+	for ( var i = n ; i >= 1; i-- )
+		result *= i;
+console.log(n + "!" + "=" + result);
+	
 //Task 3
+
 function Hayes (y) {
 	var steps = 0;
 	var topseq = 0;
@@ -30,8 +39,8 @@ function Hayes (y) {
 	console.log(steps + " " + "Шагов было сделано");
 	console.log("Вершина -" + " " + topseq);
 }
+
 var t = prompt("Введите число");
 console.log(Hayes(t));	
-	
 
 	
