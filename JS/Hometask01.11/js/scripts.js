81ee262c6887a4a78de32148ca23a7fcac81c776
RomanGvozdov <romangvozdev00@gmail.com>
2017-11-01 12:53:52 +0200

    HT

---
+++ JS/Hometask01.11/js/scripts.js
@@ -0,0 +1,37 @@
+//Task 1
+var result = 0;
+for (var i = 1; i <= 100; i++)
+	result+=i;
+console.log(result);
+//Task 2
+function factorial(n) {
+	if ( n > 1 ){
+		return(n * factorial(n - 1));
+	} else 
+		return(1);
+}
+var n = prompt("Введите число");
+console.log(factorial(n));
+//Task 3
+function Hayes (y) {
+	var steps = 0;
+	var topseq = 0;
+	while( y != 1) {
+		steps++;
+		if ( y > topseq )
+			topseq = y;
+		if ( (y % 2) == 0) {
+			console.log(y/=2);
+			
+		} else {
+			console.log(y = y*3 + 1);
+		}
+	}
+	console.log(steps + " " + "Шагов было сделано");
+	console.log("Вершина -" + " " + topseq);
+}
+var t = prompt("Введите число");
+console.log(Hayes(t));	
+	
+
+	
